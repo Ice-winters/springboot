c7317fa9a24db0fd47c6b6cde7d74bf8eaa62e37
icewinters <ice-winters@foxmail.com>
2019-04-28 14:30:18 +0800

    注册拦截器

--- src/main/java/com/ice/springboot/config/WebConfig.java
+++ src/main/java/com/ice/springboot/config/WebConfig.java
@@ -1,9 +1,14 @@
 package com.ice.springboot.config;
 
+import com.ice.springboot.interceptors.CustomHandlerInterceptor;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,6 +20,22 @@ import java.util.Date;
  */
 @Configuration
 public class WebConfig {
+    @Bean
+    public WebMvcConfigurer customMvcConfigurerAdapter (){
 
+        return new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+
+            }
+
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(new CustomHandlerInterceptor()).addPathPatterns("/**");
+                // 添加拦截器
+//                super.addInterceptors(registry);
+            }
+        };
+    }
 
 }
